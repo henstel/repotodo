66efe415a3d0181c1471c57eba4dbf4016c9dec2
henstel <henstel2013@gmail.com>
2021-10-23 12:24:05 +0100

    get data fro storage & update state using setState

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -56,11 +56,23 @@ class TodoContainer extends React.Component {
     })  
   }
   
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
-      .then(response => response.json())
-      .then(data => this.setState({ todos: data }));
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
   }
+  
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  }
+  
   
   render() {
     return (
